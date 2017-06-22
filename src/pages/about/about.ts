4ba8e00c9dbe29b773e7c407e501c1b75de0cadb
morinaga-na <morinaga@natsumeatari.co.jp>
2017-06-23 08:51:28 +0900

    追加

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,45 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+	selector: 'page-about',
+	templateUrl: 'about.html',
+	providers: [BarcodeScanner, Camera]
 })
 export class AboutPage {
+	imgSrc: string;
+	constructor(public navCtrl: NavController, private barcodeScanner: BarcodeScanner, private camera: Camera) {
+	}
 
-  constructor(public navCtrl: NavController) {
+	click_qr() {
+		this.barcodeScanner.scan()
+			.then((result) => {
+				alert(
+					"We got a barcode\n" +
+					"Result: " + result.text + "\n" +
+					"Format: " + result.format + "\n" +
+					"Cancelled: " + result.cancelled
+				)
+			})
+			.catch((error) => {
+				alert(error);
+			})
+	}
 
-  }
-
+	click_camera() {
+		const options: CameraOptions = {
+      quality: 75,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+    this.camera.getPicture(options)
+      .then((imageData) => {
+        let base64Image = 'data:image/jpeg;base64,' + imageData; this.imgSrc = base64Image;
+      }).catch((err) => {
+        console.log(err);
+      });
+	}
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,66 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation, GeolocationOptions } from '@ionic-native/geolocation';
+import { HTTP } from '@ionic-native/http';
+
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers: [Geolocation, HTTP]
 })
 export class HomePage {
+  //Flags
+  isLocAvail: boolean;
+  isLocError: boolean;
 
-  constructor(public navCtrl: NavController) {
+  // options
+  enableHighAccuracy: boolean = false;
+
+  //Results
+  loc_lng: number;
+  loc_lat: number;
+  rev_geo: string;
+
+  constructor(private geolocation: Geolocation, private http: HTTP, public navCtrl: NavController) {
+
+  }
+
+  initFlags() {
+    this.isLocAvail = false;
+    this.isLocError = false;
+  }
+
+  ngOnInit() {
+    this.initFlags();
+  }
+
+  onHighAccuracyChange(t: boolean) {
+    this.enableHighAccuracy = t;
+  }
+
+  getLocation() {
+    this.initFlags();
+
+    // option
+    let options: GeolocationOptions = {
+      enableHighAccuracy: this.enableHighAccuracy
+    };
+
+    //  緯度経度
+    this.geolocation.getCurrentPosition(options)
+      .then((resp)=>{
+        this.loc_lng = resp.coords.longitude;
+        this.loc_lat = resp.coords.latitude;
+
+        this.isLocAvail = true;
+        this.isLocError = false;
+      })
+      .catch((err) => {
+        this.isLocAvail = false;
+        this.isLocError = true;
+      });
+
 
   }
 
