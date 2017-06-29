00018080daae1d2ca6383b92571371259c7d1fa6
morinaga-na <morinaga@natsumeatari.co.jp>
2017-06-29 10:03:40 +0900

    追加

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { Flashlight } from '@ionic-native/flashlight';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html',
-  providers: [BarcodeScanner]
+  providers: [BarcodeScanner, Flashlight]
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController, private barcodeScanner: BarcodeScanner) {
+  constructor(public navCtrl: NavController, private barcodeScanner: BarcodeScanner, private flashlight: Flashlight) {
 
   }
 
@@ -26,6 +27,15 @@ export class ContactPage {
         .catch((error) => {
           alert(error);
         })
+  }
+  click_flash_on() {
+      console.log(alert(JSON.stringify(this.flashlight.isSwitchedOn())));
+      alert(this.flashlight.isSwitchedOn());
+      // alert(this.flashlight.switchOn() );
+  }
+
+  click_flash_off() {
+      this.flashlight.switchOff();
   }
 
 }
